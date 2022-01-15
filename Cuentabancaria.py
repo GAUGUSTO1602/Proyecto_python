4b46e90c26bfaad7e1bb93862ad2c0cf20296665
unknown <g.gabriel@correo.unimet.edu.ve>
2022-01-15 16:05:48 -0400

    ejercicio1

---
+++ Cuentabancaria.py
@@ -0,0 +1,39 @@
+import datetime
+from datetime import timedelta
+fecha = datetime.date(2020, 4, 10)
+fecha1 = datetime.date(2020, 4, 10)
+fecha2 = datetime.date(2020, 4, 11)
+fecha3= datetime.date(2020, 4, 17)
+fecha4 = datetime.date(2020, 4, 30)
+fecha_final = datetime.date(2020, 5, 2)
+Saldo_Inicial = int(3480)
+
+while fecha < fecha_final:
+    if fecha == fecha1:
+      print(fecha,':El saldo disponible es de $',Saldo_Inicial)
+      fecha = fecha + timedelta(days=1)
+    elif fecha == fecha2:
+      print('Se han retirado $ 96')
+      Saldo_restante = Saldo_Inicial - int(96)
+      print('Saldo actual a la fecha',fecha,': $',Saldo_restante)
+      fecha = fecha + timedelta(days=1)
+    elif fecha == fecha3:
+      print('Se ha transferido a la cuenta $ 1200')
+      Saldo_restante = Saldo_restante + int(1200)
+      print('Saldo actual a la fecha',fecha,': $',Saldo_restante)
+      fecha = fecha + timedelta(days=1)
+    elif fecha == fecha4:
+      print('Se ha abonado a su cuenta un 3% de intereses')
+      Intereses = float(3)
+      Saldo =  float(Saldo_restante)
+      Saldo_restante = Intereses * Saldo / 100 + Saldo_restante
+      Saldo_restante = round(Saldo_restante, 2)
+      print('Saldo actual a la fecha',fecha,': $',Saldo_restante)
+      fecha = fecha + timedelta(days=1)
+    else:
+      fecha = fecha + timedelta(days=1)
+
+if fecha == fecha_final:
+  print('Se han retirado $ 51')
+  Saldo_restante = Saldo_restante - int(51)
+  print('Saldo actual a la fecha',fecha,': $',Saldo_restante)
