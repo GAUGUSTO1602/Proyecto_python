c973b15d2cd00e8ec5242f028aff72c3e18ac8d6
unknown <g.gabriel@correo.unimet.edu.ve>
2022-01-15 16:04:21 -0400

    codigofecha

--- pruebafecha.py
+++ pruebafecha.py
@@ -1,3 +1,12 @@
 import datetime
+from datetime import timedelta
 fecha = datetime.date(2021, 7, 22)
 print(fecha)
+fecha2 = fecha + timedelta(days=2)
+print(fecha2)
+
+if fecha < fecha2:
+    print('funciona!')
+else:
+    print('no funciona :c')
+
