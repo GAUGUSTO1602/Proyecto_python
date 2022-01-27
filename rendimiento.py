11f858703faaeb91273889e361a6cea9adf1a529
unknown <g.gabriel@correo.unimet.edu.ve>
2022-01-27 12:08:22 -0400

    Ejercicio rendimiento

---
+++ rendimiento.py
@@ -0,0 +1,17 @@
+employee = input('Please give the name of the employee\n--> ')
+score = input('What was his/her score this year?\n--> ')
+score = float(score)
+if score == 0.0:
+    remuneration = 2400 * score
+    print('The performance of', employee, 'was unnaceptable')
+    print('His/Her remuneration shall be:', remuneration)
+elif score == 0.4:        
+    remuneration = 2400 * score
+    print('The performance of', employee, 'was aceptable')
+    print('His/Her remuneration shall be:', remuneration)
+elif score >= 0.6:
+    remuneration = 2400 * score
+    print('The performance of', employee, 'was meritorius')
+    print('His/Her remuneration shall be:', remuneration)
+else:
+    print('The information is not valid') 
