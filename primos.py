01fb2ba802204adc37ae207fcb62089f1988ff22
unknown <g.gabriel@correo.unimet.edu.ve>
2022-02-05 20:17:30 -0400

    verficar primo

--- primos.py
+++ primos.py
@@ -1,11 +1,15 @@
-num = input('Dame un numero: ')
-num = int(num)
-flag = False
-for x in range(2, num):
-    if num % x == 0:
-        flag = True
-        break
-if flag == True:
-    print('No es primo')
-else:
-    print('Es primo')
+def main():
+
+    num = input('Dame un numero: ')
+    num = int(num)
+    flag = False
+    for x in range(2, num):
+        if num % x == 0:
+            flag = True
+            break
+    if flag == True:
+        print('No es primo')
+    else:
+        print('Es primo')
+
+main()
